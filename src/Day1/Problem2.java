d0dd86f55c726bbc87e6d6818c64b6097c1d4af9
vinayux8055 <vinayux8005@gmail.com>
2026-05-24 01:21:31 +0530

    day1 problem 3

---
+++ src/Day1/Problem2.java
@@ -0,0 +1,8 @@
+package Day1;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        System.out.println("VINAYUX");
+
+    }
+}
---
+++ src/Day1/Problem3.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        int a = 10 ;
+
+        int b = 20 ;
+
+        int sum = a + b ;
+
+        System.out.println(sum);
+    }
+}
+
